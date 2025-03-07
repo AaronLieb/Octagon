101f3a597a37f0fe2b554188e95c895f16359536
Aaron Lieberman <aarolieb@amazon.com>
2025-03-07 11:40:44 -0800

    Graphic command boilerplate

---
+++ commands/graphic/graphic.go
@@ -0,0 +1,36 @@
+package seed
+
+import (
+	"context"
+
+	"github.com/charmbracelet/log"
+	"github.com/urfave/cli/v3"
+)
+
+func Command() *cli.Command {
+	return &cli.Command{
+		Name:    "graphic",
+		Usage:   "Generate a top8 graphic",
+		Aliases: []string{"g"},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "tournament",
+				Aliases: []string{"t"},
+				Usage:   "The slug for the tournament. Example: octagon-99",
+				Value:   "octagon",
+			},
+			&cli.BoolFlag{
+				Name:    "redemption",
+				Aliases: []string{"r"},
+				Usage:   "Whether you are seeding for redemption bracket or not",
+			},
+		},
+		Action: graphic,
+	}
+}
+
+func graphic(ctx context.Context, cmd *cli.Command) error {
+	log.Debug("graphic")
+
+	return nil
+}
