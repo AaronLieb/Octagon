547424e80742bedb96c9223d48c52f69fee5780b
Aaron Lieberman <aarolieb@amazon.com>
2025-04-22 14:56:44 -0700

    chore: manually update last tournament slug

--- cmd/bracket/seed.go
+++ cmd/bracket/seed.go
@@ -60,10 +60,10 @@ func seed(ctx context.Context, cmd *cli.Command) error {
 
 	// TODO: Don't hard code this, dynamically generate octagon numbers
 	event := "ultimate-singles"
-	consEvent := "tournament/octagon-107/event/ultimate-singles"
+	consEvent := "tournament/octagon-108/event/ultimate-singles"
 	if cmd.Bool("redemption") {
 		event = "redemption-bracket"
-		consEvent = "tournament/octagon-108/event/ultimate-singles"
+		consEvent = "tournament/octagon/event/ultimate-singles"
 	}
 
 	cons := conflicts.CreateConflictsForSetsPlayed(ctx, consEvent)
